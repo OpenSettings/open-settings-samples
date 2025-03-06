3ef5812a0fd5014405f4bb05453b392e41fb54ac
Oğulcan TURAN <ismetogulcanturan@gmail.com>
2025-03-06 01:47:23 +0100

    Added 1_QuickStart

---
+++ versions/v1/1_QuickStart/src/OpenSettings.Api/MyFirstSetting.cs
@@ -0,0 +1,11 @@
+﻿using OpenSettings.Services.Interfaces;
+
+namespace OpenSettings.Api
+{
+    public class MyFirstSetting : ISettings
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
